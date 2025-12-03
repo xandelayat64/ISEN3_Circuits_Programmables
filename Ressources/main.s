cfdef06b8013c2897265aa1329e526261829ab89
xandlt_ <96818249+xandelayat64@users.noreply.github.com>
2025-12-03 15:31:01 +0100

    Update main.s

--- Ressources/main.s
+++ Ressources/main.s
@@ -1,1 +1,41 @@
+PROCESSOR 18F25K40
+#include <xc.inc>
 
+; Configuration ================================================================
+config FEXTOSC = OFF           ; Pas de source d'horloge externe
+config RSTOSC = HFINTOSC_64MHZ ; Horloge interne de 64 MHz
+config WDTE = OFF              ; Desactiver le watchdog	timer
+	
+PSECT   code, abs
+   
+; Vecteur de reset =============================================================
+org     0x000
+goto init 
+   
+; Vecteur d'interruption haute priorite ========================================
+org     0x008
+goto High_ISR 
+
+; Vecteur d'interruption basse priorite ========================================
+org     0x018
+goto Low_ISR 
+
+; Programme principal ==========================================================
+org 0x100   
+
+init:
+    ; Initialisation    
+    goto loop
+
+loop:
+    ; Boucle infinie
+    goto loop
+
+; Routines d'interruption ======================================================    
+High_ISR:
+    retfie
+    
+Low_ISR:  
+    retfie
+     
+end
